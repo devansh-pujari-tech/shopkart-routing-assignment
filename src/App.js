c02a62bf12f7304c07df5725db344510c3af5cb4
Devansh <devansh@example.com>
2026-05-08 14:50:14 +0530

    Completed ShopKart Routing Assignment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
+
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Home from "./pages/Home";
-// TODO: Import About
-// TODO: Import Contact
-// TODO: Import React Router
 
 function App() {
   return (
     <div>
       <Navbar />
-      
-      {/* TODO: Implement Routing Here */}
-      
-      <Home />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      {/* TODO: Replace <a> with Link */}
-      
-      <a href="/">Home</a>
-      <a href="/about">About</a>
-      <a href="/contact">Contact</a>
-      
+    <nav style={{ padding: "10px", background: "#eee" }}>
+      {/* Replacing static elements with React router Link components */}
+      <Link to="/" style={{ margin: "0 10px" }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ margin: "0 10px" }}>
+        About
+      </Link>
+      <Link to="/contact" style={{ margin: "0 10px" }}>
+        Contact
+      </Link>
     </nav>
   );
 }
